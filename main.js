d1e617857c0436d12f126c4a153090b706534f30
strdnl <46500009+strdnl@users.noreply.github.com>
2019-02-23 17:02:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 var endpoint = "http://www.jsonstore.io/3f372cbb892d082a509f9484d4ea8f7426b30954d1a6cb2255e0a19402bbf419";
 
-function encrypt(){
-password = document.getElementById('urlinput').value;
-var xorKey = 129; /// you can have other numeric values also.
-    var result = "";
-    for (i = 0; i < password.length; ++i) {
-        result += String.fromCharCode(xorKey ^ password.charCodeAt(i));
-        return result;
-    }
-}
-
-function decrypt(url){
-password = url;
-var xorKey = 129; /// you can have other numeric values also.
-    var result = "";
-    for (i = 0; i < password.length; ++i) {
-        result += String.fromCharCode(xorKey ^ password.charCodeAt(i));
-        return result;
-    }
-}
-
 function geturl(){
     var url = document.getElementById("urlinput").value;
     var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://") || url.startsWith("data:text/");
@@ -49,12 +29,11 @@ function genhash(){
 
 function send_request(url) {
      var codex = CryptoJS.AES.encrypt(url, window.location.hash.substr(1));
-     
-    this.url = codex;
+     this.url = codex;
     $.ajax({
         'url': endpoint + "/" + window.location.hash.substr(1),
         'type': 'POST',
-        'data': JSON.stringify (this.url),
+        'data': JSON.stringify(this.url),
         'dataType': 'json',
         'contentType': 'application/json; charset=utf-8'
 })
@@ -71,9 +50,10 @@ var hashh = window.location.hash.substr(1)
 if (window.location.hash != "") {
     $.getJSON(endpoint + "/" + hashh, function (data) {
         data = data["result"];
+        var decrypted = CryptoJS.AES.decrypt(data, window.location.hash.substr(1));
 
-        if (data != null) {
-            var decrypted = CryptoJS.AES.decrypt(data, window.location.hash.substr(1));
+        if (decrypted != null) {
+            
             window.location.href = decrypted.toString(CryptoJS.enc.Utf8);
         }
 
